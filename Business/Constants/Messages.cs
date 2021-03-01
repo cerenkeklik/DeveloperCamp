23f8e1075921c863f83c4ea0369211468289d04d
Ceren <48736578+cerenkeklik@users.noreply.github.com>
2021-03-01 19:49:53 +0300

    Utilities and constant messages

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //publicler variable olsa dahi pascal case yazılır
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Product name invalid.";
+        public static string MaintenanceTime = "System in maintenance time.";
+        public static string ProductsListed = "Products listed.";
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message): this(success)
+        {
+            //Read-only ler constructorda set edilebilir.
+          
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            //Read-only ler constructorda set edilebilir.
+            Success = success;
+           
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
